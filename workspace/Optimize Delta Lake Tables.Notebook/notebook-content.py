9e975a31ef802dc86be1c0eb1fe3b53c9ad9cf25
Brenton McPhee <brenton.mcphee@clouddirect.net>
2025-04-14 15:35:21 +0200

    Fix uids

--- workspace/Optimize Delta Lake Tables.Notebook/notebook-content.py
+++ workspace/Optimize Delta Lake Tables.Notebook/notebook-content.py
@@ -10,7 +10,7 @@
 # META     "lakehouse": {
 # META       "default_lakehouse": "cc80a0ab-603d-4df9-bdfc-c35a7e8ab095",
 # META       "default_lakehouse_name": "lh_silver",
-# META       "default_lakehouse_workspace_id": "8d8d00a7-0e8a-4e3b-8c0e-8dcafac7adec"
+# META       "default_lakehouse_workspace_id": "5acf5e70-08fd-48ba-9acc-dc997b845117"
 # META     }
 # META   }
 # META }
